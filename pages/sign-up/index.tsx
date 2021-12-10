03ebbe2fd64bee90b6e9c47cb9f03cdc43621d23
Paulina <p.kozikowska@rits.center>
2021-12-10 17:39:14 +0100

    Init commit

---
+++ pages/sign-up/index.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+
+const Index: FC = () => {
+  return (
+    <div>
+      <div>
+
+      </div>
+      <div>
+        <div>First, let's make sure we serve your</div>
+        <input placeholder="zipcode"/>
+      </div>
+    </div>
+  )
+}
+
+export default Index;
